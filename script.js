092cf992861cbcb697ab2ea0ffaeac6e3eab5273
eduardo <eduardocastroemp@gmail.com>
2022-07-02 17:18:49 -0300

    Adicionado Elemento "Share"

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+
+/* Login */
 ativar.addEventListener('click', () =>{
     modal.classList.toggle("animar");
 })
 
+/* share */
+share.addEventListener('click', () =>{
+    insta.classList.toggle("c-insta");
+    github.classList.toggle("c-github");
+    linkedin.classList.toggle("c-linkedin");
+  })
