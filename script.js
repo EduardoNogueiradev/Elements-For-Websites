787f18fde95f88ab6951fdf37a55d2d57c33aea5
eduardo <eduardocastroemp@gmail.com>
2022-07-03 23:28:43 -0300

    adicionado cards

--- script.js
+++ script.js
@@ -10,3 +10,61 @@ share.addEventListener('click', () =>{
     github.classList.toggle("c-github");
     linkedin.classList.toggle("c-linkedin");
   })
+
+
+  /* js cards */
+  /* pode ser usado para mostrar elementos quando vizualizados na tela */
+  const observer = new IntersectionObserver( entries => {
+    console.log(entries)
+  
+    Array.from(entries).forEach(entry => {
+      if (entry.intersectionRatio >= 1) {
+      entry.target.classList.add('init-hidden-off')
+      }
+    })
+   
+  }, {
+   threshold: 1
+  })
+  Array.from(document.querySelectorAll('.init-hidden')).forEach( element => {
+  observer.observe(element)
+  })
+  
+
+
+  const observer1 = new IntersectionObserver( entries => {
+    console.log(entries)
+  
+    Array.from(entries).forEach(entry => {
+      if (entry.intersectionRatio >= 1) {
+      entry.target.classList.add('mov')
+      }
+    })
+   
+  }, {
+   threshold: 1
+  })
+  Array.from(document.querySelectorAll('.no-mov')).forEach( element => {
+  observer1.observe(element)
+  })
+
+    const observer2 = new IntersectionObserver( entries => {
+    console.log(entries)
+  
+    Array.from(entries).forEach(entry => {
+      if (entry.intersectionRatio >= 1) {
+      entry.target.classList.add('mov-t')
+      }
+    })
+   
+  }, {
+   threshold: 1
+  })
+  Array.from(document.querySelectorAll('.no-mov-t')).forEach( element => {
+  observer2.observe(element)
+  })
+  
+  
+  
+  
+  
